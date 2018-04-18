c9b021f7efb8a070fcb10cf6d35435db18c53fba
dak5374 <dak5374@psu.edu>
2018-04-18 07:44:08 -0400

    Delete MeaghanPaigeInventorySystem.java

--- MeaghanPaigeInventorySystem/src/meaghanpaigeinventorysystem/MeaghanPaigeInventorySystem.java
+++
@@ -1,8 +0,0 @@
-package meaghanpaigeinventorysystem;
-
-public class MeaghanPaigeInventorySystem {
-
-    public static void main(String[] args) {
-        //TODO -- App code here
-    } 
-}//End Main
