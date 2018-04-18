3a97dadfd05591d637c95cc7e0cf723dc4e3770a
dak5374 <dak5374@psu.edu>
2018-04-17 21:10:18 -0400

    Created basic UI with tabs. size to be modified later. Tabs to be added.

---
+++ MeaghanPaigeInventorySystem/src/Control/MeaghanPaigeInventorySystem.java
@@ -0,0 +1,14 @@
+package Control;
+
+import UI.WelcomePane;
+import javax.swing.JFrame;
+
+public class MeaghanPaigeInventorySystem {
+
+    public static void main(String[] args) {
+        WelcomePane wp = new WelcomePane();
+        wp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        wp.setSize(600,400);
+        wp.setVisible(true);
+    } 
+}//End Main
