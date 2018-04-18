39ffe4e75d25f45cc44d2cdbb9dc0728675a17a5
dak5374 <dak5374@psu.edu>
2018-04-18 16:30:19 -0400

    Renamed buttons to be less ambiguous.

---
+++ MeaghanPaigeInventorySystem/src/View/ImportPanel.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+/**
+ *
+ * @author usmck
+ */
+public class ImportPanel extends javax.swing.JPanel {
+
+    /**
+     * Creates new form ImportPanel
+     */
+    public ImportPanel() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        importLabel = new javax.swing.JLabel();
+        squareSpaceButton = new javax.swing.JButton();
+        payPalButton = new javax.swing.JButton();
+        promptLabel = new javax.swing.JLabel();
+        manualButton = new javax.swing.JButton();
+
+        importLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        importLabel.setText("Import");
+
+        squareSpaceButton.setText("SquareSpace");
+
+        payPalButton.setText("PayPal");
+
+        promptLabel.setText("Select an import source below");
+
+        manualButton.setText("Manual");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(importLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(promptLabel)
+                                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                                                .addComponent(manualButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                .addComponent(payPalButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                .addComponent(squareSpaceButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                                .addGap(0, 157, Short.MAX_VALUE)))
+                                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(importLabel)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(promptLabel)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(squareSpaceButton)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(payPalButton)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(manualButton)
+                                .addContainerGap(114, Short.MAX_VALUE))
+        );
+    }
+
+    private javax.swing.JButton squareSpaceButton;
+    private javax.swing.JButton payPalButton;
+    private javax.swing.JButton manualButton;
+    private javax.swing.JLabel importLabel;
+    private javax.swing.JLabel promptLabel;
+                
+}
