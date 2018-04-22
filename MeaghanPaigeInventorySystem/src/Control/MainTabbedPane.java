9c86feeac166764206755c1d08b145b4a87047b6
usmck <usmck@104.39.126.134>
2018-04-22 13:22:05 -0400

    moved and refactored tabbedpane to control folder

--- MeaghanPaigeInventorySystem/src/Control/MainTabbedPane.java
+++ MeaghanPaigeInventorySystem/src/Control/MainTabbedPane.java
@@ -3,8 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package View;
+package Control;
 
+import View.ImportPanel;
+import View.InventoryPanel;
+import View.WelcomePanel;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
