e0dce1e9222f7e25fb57ce178b4650cb2e188703
Rick Orlando <rko5026@psu.edu>
2018-04-23 16:53:16 -0400

    - Finished form and named all variables

--- MeaghanPaigeInventorySystem/src/View/ClientPanel.java
+++ MeaghanPaigeInventorySystem/src/View/ClientPanel.java
@@ -14,38 +14,38 @@ public class ClientPanel extends javax.swing.JPanel {
 
     public ClientPanel() {
 
-        jLabel1 = new javax.swing.JLabel();
-        clientName = new javax.swing.JTextField();
-        clientAddress = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
-        jTextField6 = new javax.swing.JTextField();
-        jTextField7 = new javax.swing.JTextField();
-        jTextField8 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        lblAddClient = new javax.swing.JLabel();
+        tboxClientName = new javax.swing.JTextField();
+        tboxClientAddress = new javax.swing.JTextField();
+        tboxClientCity = new javax.swing.JTextField();
+        tboxClientState = new javax.swing.JTextField();
+        tboxClientZip = new javax.swing.JTextField();
+        tboxClientPhone = new javax.swing.JTextField();
+        tboxClientFax = new javax.swing.JTextField();
+        tboxClientEmail = new javax.swing.JTextField();
+        btnAddClient = new javax.swing.JButton();
         
 
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Add Client");
+        lblAddClient.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblAddClient.setText("Add Client");
 
-        clientName.setText("Client Name");
+        tboxClientName.setText("Client Name");
 
-        clientAddress.setText("Client Address");
+        tboxClientAddress.setText("Client Address");
 
-        jTextField3.setText("Client City");
+        tboxClientCity.setText("Client City");
 
-        jTextField4.setText("Client State");
+        tboxClientState.setText("Client State");
 
-        jTextField5.setText("Client ZipCode");
+        tboxClientZip.setText("Client ZipCode");
 
-        jTextField6.setText("Client Phone Number");
+        tboxClientPhone.setText("Client Phone Number");
 
-        jTextField7.setText("Client Fax Number");
+        tboxClientFax.setText("Client Fax Number");
 
-        jTextField8.setText("Client Email");
+        tboxClientEmail.setText("Client Email");
 
-        jButton1.setText("Add Client");
+        btnAddClient.setText("Add Client");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -54,61 +54,61 @@ public class ClientPanel extends javax.swing.JPanel {
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(lblAddClient, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addGroup(layout.createSequentialGroup()
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                                        .addComponent(jTextField7)
-                                                        .addComponent(clientName)
-                                                        .addComponent(jTextField3)
-                                                        .addComponent(jTextField5))
+                                                        .addComponent(tboxClientFax)
+                                                        .addComponent(tboxClientName)
+                                                        .addComponent(tboxClientCity)
+                                                        .addComponent(tboxClientZip))
                                                 .addGap(160, 160, 160)
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                                        .addComponent(clientAddress)
-                                                        .addComponent(jTextField4)
-                                                        .addComponent(jTextField6)
-                                                        .addComponent(jTextField8))
+                                                        .addComponent(tboxClientAddress)
+                                                        .addComponent(tboxClientState)
+                                                        .addComponent(tboxClientPhone)
+                                                        .addComponent(tboxClientEmail))
                                                 .addGap(0, 0, Short.MAX_VALUE)))
                                 .addContainerGap())
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(166, 166, 166)
-                                .addComponent(jButton1)
+                                .addComponent(btnAddClient)
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jLabel1)
+                                .addComponent(lblAddClient)
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(clientName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(clientAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(tboxClientName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(tboxClientAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(36, 36, 36)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(tboxClientCity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(tboxClientState, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(36, 36, 36)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(tboxClientZip, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(tboxClientPhone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(36, 36, 36)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(jTextField7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jTextField8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(tboxClientFax, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(tboxClientEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton1)
+                                .addComponent(btnAddClient)
                                 .addContainerGap(25, Short.MAX_VALUE))
         );
     }
     
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField clientName;
-    private javax.swing.JTextField clientAddress;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
-    private javax.swing.JTextField jTextField7;
-    private javax.swing.JTextField jTextField8;
+    private javax.swing.JButton btnAddClient;
+    private javax.swing.JLabel lblAddClient;
+    private javax.swing.JTextField tboxClientName;
+    private javax.swing.JTextField tboxClientAddress;
+    private javax.swing.JTextField tboxClientCity;
+    private javax.swing.JTextField tboxClientState;
+    private javax.swing.JTextField tboxClientZip;
+    private javax.swing.JTextField tboxClientPhone;
+    private javax.swing.JTextField tboxClientFax;
+    private javax.swing.JTextField tboxClientEmail;
 }
